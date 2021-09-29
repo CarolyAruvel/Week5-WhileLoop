c69bfe6fc3f6eaf05311645bcf5dec3ed20db3df
Opilane TTHK <opilane@tthk.ee>
2021-09-29 09:53:39 +0300

    WhileLoop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada PIN kood
+            //programm võrdleb sisestatud PIN koodi arvuga 1234
+            //kui sisestatud PIN kood on 1234
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui sisestatud PIN kood on vale, programm kuvab konsoolis
+            //"Vale PIN. Proovi uuesti"
+            //katsete arv on piiramatu
+
+            //boolean --> true/false
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Sisesta PIN:");
+                int userPIN = Convert.ToInt32(Console.ReadLine());
+
+                if(userPIN == 1234)
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    loopActive = false;
+                }
+
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
